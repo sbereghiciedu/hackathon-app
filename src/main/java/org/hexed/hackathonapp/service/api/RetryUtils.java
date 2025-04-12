936161298c89d783f30615939039ca7edd5263da
vh <vhernest@gmail.com>
2025-04-12 13:40:01 +0300

    add refresh token support

--- src/main/java/org/hexed/hackathonapp/service/api/RetryUtils.java
+++ src/main/java/org/hexed/hackathonapp/service/api/RetryUtils.java
@@ -1,8 +1,15 @@
 package org.hexed.hackathonapp.service.api;
 
+import org.hexed.hackathonapp.controller.DebugController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
 import java.util.function.Supplier;
 
 public class RetryUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(DebugController.class);
 
     public static <T> T retry(Supplier<T> supplier, int maxRetries) {
         int attempts = 0;
@@ -10,11 +17,15 @@ public class RetryUtils {
             try {
                 T result = supplier.get();
                 if (result instanceof Integer && (Integer) result < 0) {
+                    logger.info("Invalid result: negative. Retrying");
                     throw new RuntimeException("Retry triggered: result < 0");
                 }
 
                 return result;
+            } catch (WebClientResponseException.Unauthorized e) {
+                logger.info("Unauthorized, refresh token");
             } catch (Exception e) {
+                logger.info("Server error, retrying");
                 attempts++;
                 if (attempts > maxRetries) {
                     throw e;
